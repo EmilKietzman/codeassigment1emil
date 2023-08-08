c4a57e8b94b4cb19639e437649df134a08931686
EmilKietzman <135075052+EmilKietzman@users.noreply.github.com>
2023-08-08 20:22:40 +0200

    Update dao.ts

--- typedContracts/dao/event-types/dao.ts
+++ typedContracts/dao/event-types/dao.ts
@@ -1,3 +1,3 @@
-import type {ReturnNumber} from "@727-ventures/typechain-types";
+import type {ReturnNumber} from "@Brushfam/typechain-types";
 import type * as ReturnTypes from '../types-returns/dao';
 
